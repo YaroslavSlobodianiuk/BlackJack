2376cf473bfdfc6a930019dbeb248b5f0485b210
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-21 21:59:57 +0300

    GetWinnerClass.java

---
+++ GetWinnerClass.java
@@ -0,0 +1,24 @@
+
+
+public class GetWinnerClass {
+    public int getWinner(int[] arrayOfResult, int numberOfPlayer) {
+
+        int winner = -1;
+        int maxValue = 0;
+        for (int i = 0; i < numberOfPlayer; i++) {
+            if (arrayOfResult[i] < 22 && arrayOfResult[i] > maxValue){
+                System.out.println(arrayOfResult[i] + " arrayOfResult");
+                System.out.println(maxValue + " maxValue");
+                maxValue = arrayOfResult[i];
+                winner = i;
+            }
+        }
+        if (winner == -1){
+            System.out.println("Everyone lost");
+        } else {
+            winner++;
+            System.out.println("Player " + winner + " won");
+        }
+        return winner;
+    }
+}
