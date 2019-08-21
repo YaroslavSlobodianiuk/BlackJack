c07e7609b5ea59bca9e6c16ddfb18ede85ad2547
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-21 21:58:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,65 +10,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         RandomClass random = new RandomClass();
+        GetWinnerClass getWinnerClass = new GetWinnerClass();
         int result = 0;
         System.out.println("BlackJack");
         System.out.println("Enter number of players");
         int numberOfPlayer;
         numberOfPlayer = in.nextInt();
-        int ArrayOfResults[] = new int[numberOfPlayer];
+        int[] arrayOfResults = new int[numberOfPlayer];
         for (int k = 1; k < numberOfPlayer+1; k++){
             System.out.println("Player " + k);
-        System.out.println("- In order to receive cards tap 1\n- Exit - 0");
-        if (in.nextInt() == 1) {
-            for (int i = 0;i < 2;i++){
+            System.out.println("- In order to receive cards tap 1\n- Exit - 0");
+            if (in.nextInt() == 1) {
+                for (int i = 0;i < 2;i++){
 
-                int sum[] = new int[2];
-                sum[i] = random.getRandArrayElement();
-                System.out.println(sum[i]);
-                result += sum[i];
-                if (result == 22){
+                    int sum[] = new int[2];
+                    sum[i] = random.getRandArrayElement();
+                    System.out.println(sum[i]);
+                    result += sum[i];
+                    if (result == 22){
                     System.out.println("Gold Jack");
                 }
 
             }
             System.out.println(result);
-        } else {
-        System.out.println("Finish!");
-        }
+            } else {
+                System.out.println("Finish!");
+            }
         while (true) {
             System.out.println("- Receive one more card! - 1\n- Finish - 0");
             if (in.nextInt() == 0) break;
-            int test = random.getRandArrayElement();
-            System.out.println(test);
-            result +=test;
-            System.out.println(result);
+                int test = random.getRandArrayElement();
+                System.out.println(test);
+                result +=test;
+                System.out.println(result);
             if (result > 21) {
                 System.out.println("You lost");
                 break;
             }
         }
         System.out.println("Finish! You have " + result + " points!");
-        ArrayOfResults[k-1] = result - 21;
+        arrayOfResults[k-1] = result;
         result = 0;
     }
-        if (ArrayOfResults[0] < 1 && ArrayOfResults[1] < 1 && ArrayOfResults[0] == ArrayOfResults[1]){
-            System.out.println("DRAW");
-        }
-        if (ArrayOfResults[0] > 1 && ArrayOfResults[1] > 1) {
-            System.out.println("Both lost");
-        }
-        if (ArrayOfResults[0] < 1 && ArrayOfResults[1] < 1 && ArrayOfResults[0] > ArrayOfResults[1]) {
-            System.out.println("Player 1 won");
-        }
-        if (ArrayOfResults[0] < 1 && ArrayOfResults[1] < 1 && ArrayOfResults[0] < ArrayOfResults[1]) {
-            System.out.println("Player 2 won");
-        }
-        if (ArrayOfResults[0] > 0 && ArrayOfResults[1] < 1) {
-            System.out.println("Player 2 won");
-        }
-        if (ArrayOfResults[0] < 1 && ArrayOfResults[1] > 0) {
-            System.out.println("Player 1 won");
-        }
+        getWinnerClass.getWinner(arrayOfResults, numberOfPlayer);
+
     }
 
 }
