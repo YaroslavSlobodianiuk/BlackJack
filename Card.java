bae8b6495df5dd25594e3728536a765d37671abc
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-26 06:12:42 +0300

    Great

--- Card.java
+++ Card.java
@@ -1,16 +1,40 @@
-public class Card {
-    private String[] suit = {"spades", "clubs", "hearts", "diamonds"};
-    private String[] name = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
-    String[] deck = new String[suit.length*name.length];
-    int indexOfDeck = 0;
-    public String[] createDeck() {
-        for (int i = 0; i < suit.length; i++) {
-            for (int j = 0; j < name.length; j++) {
-                deck[indexOfDeck] = suit[i] + " " + name[j];
-                indexOfDeck++;
+import java.util.*;
+
+public class Deck {
+
+
+    String[] suit = {"Spades", "Diamonds", "Hearts", "Clubs"};
+    String[] name = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    int counter = 0;
+
+
+
+    List<Card> list = new ArrayList<Card>();
+    List<String> stringList = new ArrayList<>();
+
+    public void addToList() {
+        for (int i = 0; i < suit.length; i++){
+            for (int j = 0; j < name.length; j++){
+                Card tmp = new Card(suit[i], name[j]);
+                list.add(tmp);
             }
         }
-        return deck;
+        Collections.shuffle(list);
     }
 
+
+    public Card getCard() {
+        //Card tmp = list.get(counter);
+        //list.remove(counter);
+        //return tmp;
+        Card tmp = list.get(counter);
+        list.remove(counter);
+        counter++;
+        return tmp;
+    }
+    /*public Integer test(){
+        System.out.println(list.get(0));
+            return  list.get(0).getValue();
+
+    }*/
 }
