7616c0f441e443db0c5873f9a3283ee34b5e7549
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-30 11:58:37 +0300

    List?no...array better

--- Main.java
+++ Main.java
@@ -13,14 +13,27 @@ public class Main {
 
         int result = 0;
         int numberOfPlayer;
+        int counter = 1;
+        int counterI = 0;
+        int counterJ = 0;
+        String[][] receivedCards = new String[4][16];
 
         System.out.println("BlackJack");
         System.out.println("Enter number of players");
         numberOfPlayer = in.nextInt();
         int[] arrayOfResults = new int[numberOfPlayer];
+        String[] nameOfPlayer = new String[numberOfPlayer];
 
-        for (int k = 1; k < numberOfPlayer+1; k++){
-            System.out.println("Player " + k);
+        for (int i = 0; i < numberOfPlayer; i++) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter name of " + counter + " player");
+            nameOfPlayer[i] = scanner.nextLine();
+            counter++;
+        }
+
+        for (int k = 0; k < numberOfPlayer; k++){
+            System.out.println("Player " + nameOfPlayer[k]);
             System.out.println("- In order to receive cards tap 1\n- Exit - 0");
             if (in.nextInt() == 1) {
                 for (int i = 0;i < 2;i++){
@@ -28,6 +41,8 @@ public class Main {
                     int sum[] = new int[2];
                     Card card = deck.getCard();
                     System.out.println(card);
+                    receivedCards[counterI][counterJ] = card.getSuit()+ " " + card.getName();
+                    counterJ++;
                     while (card == null) {
                         if (card == null) {
                             System.out.println("NULL");
@@ -37,7 +52,7 @@ public class Main {
                             break;
                         }
                     }
-                    sum[i] = cardRandom.defineValueOfCard(card.getName());
+                    sum[i] = card.getValue();
                     System.out.println(sum[i]);
                     result += sum[i];
                     if (result == 22){
@@ -54,6 +69,8 @@ public class Main {
                 if (in.nextInt() == 0) break;
                 Card card = deck.getCard();
                 System.out.println(card);
+                receivedCards[counterI][counterJ] = card.getSuit()+ " " + card.getName();
+                counterJ++;
                 while (card == null) {
                     if (card == null){
                         System.out.println("NULL");
@@ -63,7 +80,7 @@ public class Main {
                         break;
                     }
                 }
-                int plus = cardRandom.defineValueOfCard(card.getName());
+                int plus = card.getValue();
                 System.out.println(plus);
                 result +=plus;
                 System.out.println(result);
@@ -73,10 +90,12 @@ public class Main {
                 }
             }
             System.out.println("Finish! You have " + result + " points!");
-            arrayOfResults[k-1] = result;
+            arrayOfResults[k] = result;
             result = 0;
+            counterI++;
+            counterJ = 0;
         }
-        getWinner.getWinner(arrayOfResults, numberOfPlayer);
+        getWinner.getWinner(arrayOfResults, numberOfPlayer, nameOfPlayer, receivedCards);
 
     }
 
