25c1acfb72dea29c7b188f3efc712a3970f7b56d
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-30 11:56:26 +0300

    List?no...array better

--- Deck.java
+++ Deck.java
@@ -5,6 +5,7 @@ public class Deck {
 
     String[] suit = {"Spades", "Diamonds", "Hearts", "Clubs"};
     String[] name = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    Integer[] value = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
     int counter = 0;
 
 
@@ -15,7 +16,7 @@ public class Deck {
     public void addToList() {
         for (int i = 0; i < suit.length; i++){
             for (int j = 0; j < name.length; j++){
-                Card tmp = new Card(suit[i], name[j]);
+                Card tmp = new Card(suit[i], name[j], value[j]);
                 list.add(tmp);
             }
         }
@@ -35,6 +36,5 @@ public class Deck {
     /*public Integer test(){
         System.out.println(list.get(0));
             return  list.get(0).getValue();
-
     }*/
 }
