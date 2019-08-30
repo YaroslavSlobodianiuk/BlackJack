988c4736d735a2e9778291b62e1b54046bd810f9
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-08-30 11:55:08 +0300

    List?no...array better

--- GetWinner.java
+++ GetWinner.java
@@ -1,8 +1,10 @@
 public class GetWinner {
 
-    public int getWinner(int[] arrayOfResult, int numberOfPlayer) {
+
+    public int getWinner(int[] arrayOfResult, int numberOfPlayer, String[] nameOfPlayer, String[][] receivedCards) {
 
         int winner = -1;
+        String nameOfWinner = null;
         int maxValue = 0;
         for (int i = 0; i < numberOfPlayer; i++) {
             if (arrayOfResult[i] < 22 && arrayOfResult[i] > maxValue){
@@ -10,13 +12,17 @@ public class GetWinner {
                 System.out.println(maxValue + " maxValue");
                 maxValue = arrayOfResult[i];
                 winner = i;
+                nameOfWinner = nameOfPlayer[i];
             }
         }
         if (winner == -1){
             System.out.println("Everyone lost");
         } else {
-            winner++;
-            System.out.println("Player " + winner + " won");
+            System.out.print("Player " + nameOfWinner + " won with such numbers of cards: ");
+            for (int j = 0; j < receivedCards.length; j++){
+                if (receivedCards[winner][j] == null) break;
+                System.out.print(receivedCards[winner][j] + " ");
+            }
         }
         return winner;
     }
