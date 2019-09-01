6aacf3eda6b14de4706dc0e5e7668b543ad69b5a
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-09-01 10:49:35 +0300

    List so lists

--- GetWinner.java
+++ GetWinner.java
@@ -1,10 +1,11 @@
+import java.util.List;
+
 public class GetWinner {
 
 
-    public int getWinner(int[] arrayOfResult, int numberOfPlayer, String[] nameOfPlayer, String[][] receivedCards) {
+    public int getWinner(int[] arrayOfResult, int numberOfPlayer, List<String> concatenateListOfCardsAndPlayer) {
 
         int winner = -1;
-        String nameOfWinner = null;
         int maxValue = 0;
         for (int i = 0; i < numberOfPlayer; i++) {
             if (arrayOfResult[i] < 22 && arrayOfResult[i] > maxValue){
@@ -12,17 +13,12 @@ public class GetWinner {
                 System.out.println(maxValue + " maxValue");
                 maxValue = arrayOfResult[i];
                 winner = i;
-                nameOfWinner = nameOfPlayer[i];
             }
         }
         if (winner == -1){
             System.out.println("Everyone lost");
         } else {
-            System.out.print("Player " + nameOfWinner + " won with such numbers of cards: ");
-            for (int j = 0; j < receivedCards.length; j++){
-                if (receivedCards[winner][j] == null) break;
-                System.out.print(receivedCards[winner][j] + " ");
-            }
+            System.out.print("Player " + concatenateListOfCardsAndPlayer.get(winner) + "cards WON");
         }
         return winner;
     }
