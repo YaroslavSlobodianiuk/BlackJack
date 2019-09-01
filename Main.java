b2f4cf350883a5f3766f09cdbe9afaaa8ef6629e
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-09-01 10:48:46 +0300

    List so lists

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,32 +10,40 @@ public class Main {
         Scanner in = new Scanner(System.in);
         GetWinner getWinner = new GetWinner();
         Deck deck = new Deck();
+        PList pList = new PList();
 
+
+        List<String> playerList = new ArrayList<String>();
+        List<String> playerCards = new ArrayList<String>();
         deck.addToList();
+
 
         int result = 0;
         int numberOfPlayer;
         int counter = 1;
-        int counterI = 0;
-        int counterJ = 0;
-        String[][] receivedCards = new String[4][16];
+        int playerNumber = 0;
+
+
+
 
         System.out.println("BlackJack");
         System.out.println("Enter number of players");
         numberOfPlayer = in.nextInt();
+
         int[] arrayOfResults = new int[numberOfPlayer];
-        String[] nameOfPlayer = new String[numberOfPlayer];
+
 
         for (int i = 0; i < numberOfPlayer; i++) {
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Enter name of " + counter + " player");
-            nameOfPlayer[i] = scanner.nextLine();
+            String name = scanner.nextLine();
+            playerList.add(name);
             counter++;
         }
 
         for (int k = 0; k < numberOfPlayer; k++){
-            System.out.println("Player " + nameOfPlayer[k]);
+            System.out.println("Player " + playerList.get(k));
             System.out.println("- In order to receive cards tap 1\n- Exit - 0");
             if (in.nextInt() == 1) {
                 for (int i = 0;i < 2;i++){
@@ -41,8 +51,8 @@ public class Main {
                     int sum[] = new int[2];
                     Card card = deck.getCard();
                     System.out.println(card);
-                    receivedCards[counterI][counterJ] = card.getSuit()+ " " + card.getName();
-                    counterJ++;
+                    playerCards.add(card.getSuit().concat(" ").concat(card.getName()));
+
                     while (card == null) {
                         if (card == null) {
                             System.out.println("NULL");
@@ -52,13 +62,13 @@ public class Main {
                             break;
                         }
                     }
+
                     sum[i] = card.getValue();
                     System.out.println(sum[i]);
                     result += sum[i];
                     if (result == 22){
                         System.out.println("Gold Jack");
                     }
-
                 }
                 System.out.println(result);
             } else {
@@ -67,10 +77,11 @@ public class Main {
             while (true) {
                 System.out.println("- Receive one more card! - 1\n- Finish - 0");
                 if (in.nextInt() == 0) break;
+
                 Card card = deck.getCard();
                 System.out.println(card);
-                receivedCards[counterI][counterJ] = card.getSuit()+ " " + card.getName();
-                counterJ++;
+                playerCards.add(card.getSuit().concat(" ").concat(card.getName()));
+
                 while (card == null) {
                     if (card == null){
                         System.out.println("NULL");
@@ -90,12 +101,15 @@ public class Main {
                 }
             }
             System.out.println("Finish! You have " + result + " points!");
+            pList.concatenateCardsAndPlayer(playerCards, playerList, playerNumber);
+            playerNumber++;
+            playerCards.clear();
             arrayOfResults[k] = result;
             result = 0;
-            counterI++;
-            counterJ = 0;
+
         }
-        getWinner.getWinner(arrayOfResults, numberOfPlayer, nameOfPlayer, receivedCards);
+
+        getWinner.getWinner(arrayOfResults, numberOfPlayer, pList.getConcatenateListOfCardsAndPlayer());
 
     }
 
