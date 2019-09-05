a911d1d73f01b853bf93f7edfba0b4b95db32daf
YaroslavSlobodianiuk <43205432+YaroslavSlobodianiuk@users.noreply.github.com>
2019-09-06 00:06:38 +0300

    Done

--- Player.java
+++ Player.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-        String name;
-        List<Card> cards;
+    String name;
+    List<Card> cards;
     public Player (String name, List<Card> cards) {
         this.name = name;
         this.cards = cards;
@@ -14,5 +14,8 @@ public class Player {
     }
     public List<Card> getCards(){
         return cards;
+    }
+    public String toString() {
+        return name + " with " + cards;
     }
 }
